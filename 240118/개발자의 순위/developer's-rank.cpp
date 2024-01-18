7bd56c8442a697c9f2a050b058f9294328a8454b
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-01-18 22:59:34 +0900

    Title: 개발자의 순위, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240118/개발자의 순위/developer's-rank.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int k,n;
+    cin>>k>>n;
+    int developer[k][n];
+    for(int i=0;i<k;i++){
+        for(int j=0;j<n;j++){
+            cin>>developer[i][j];
+        }
+    }
+    int num=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            int count=0;
+            for(int l=0;l<k;l++){
+                int gab,gab2;
+                for(int a=0;a<n;a++){
+                    
+                    if(developer[l][a]==i){
+                        gab=a;
+                    }
+                    else if(developer[l][a]==j){
+                        gab2=a;
+                    }
+                }
+                if(gab<gab2){
+                    count++;
+                }
+            }
+            if(count==k){
+                num++;
+            }
+        }
+    }
+    cout<<num;
+    return 0;
+}
