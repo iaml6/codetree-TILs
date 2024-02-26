47bb8937548c892087ef820cafabe0cefa1e011b
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-02-26 22:04:04 +0900

    Title: 양수 직사각형의 최대 크기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int num[20][20]={};
+bool Yang(int a,int b, int startx,int starty){
+    for(int i=startx;i<=startx+a-1;i++){
+        for(int j=starty;j<=starty+b-1;j++){
+            if(num[i][j]<=0){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>num[i][j];
+        }
+    }
+    int hab=-1;
+    for(int a=1;a<=n;a++){
+        for(int b=1;b<=m;b++){
+            
+            for(int i=0;i<n-a+1;i++){
+                for(int j=0;j<m-b+1;j++){
+                    if(Yang(a,b,i,j)){
+                        hab=max(hab,a*b);
+                    }
+                }
+            }
+        }
+    }
+    cout<<hab;
+    return 0;
+}
