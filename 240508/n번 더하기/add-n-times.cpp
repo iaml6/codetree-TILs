c2f0594c560adb61313b74e9e78f36dc0024a472
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-05-08 15:57:45 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240508/n번 더하기/add-n-times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,n;
+    cin>>a>>n;
+    int num=a;
+    for(int i=0;i<n;i++){
+        num+=n;
+        cout<<num;
+    }
+    return 0;
+}
