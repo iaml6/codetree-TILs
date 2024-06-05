7f5c39f6c5a45bac0628a9a37d4a4f8cadea8aba
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-06-05 18:23:28 +0900

    Title: 1이 되는 순간까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240605/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int count=0;
+int Ret(int n){
+    if(n==1) {
+        if(count==0){
+            return 1;
+        }
+        return count;
+    }
+
+    if(n%2==0){
+        count++;
+        return Ret(n/2);
+    }
+    else {
+        count++;
+        return Ret(n/3);
+    }
+
+    
+    
+    
+    }
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    cout<<Ret(n);
+    return 0;
+}
