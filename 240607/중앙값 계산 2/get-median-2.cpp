26f5149ffaef7c9b72c6c169444a9d43d0636c6f
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-06-07 13:35:09 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240607/중앙값 계산 2/get-median-2.cpp
+++ 240607/중앙값 계산 2/get-median-2.cpp
@@ -6,15 +6,12 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin>>n;
-    int num[n+1];
-    for(int i=1;i<=n;i++){
+    int num[n];
+    for(int i=0;i<n;i++){
         cin>>num[i];
     }
-    for(int i=1;i<=n;i++){
-        if(i==1){
-            cout<<num[i]<<" ";
-        }
-        if(i%2==1&&i!=1){
+    for(int i=0;i<n;i++){
+        if(i%2==0){
             sort(num,num+i+1);
             cout<<num[i/2]<<" ";
         }
