1952a0be039bf5591ffe68a2612726bf41fc6b29
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-06-13 16:06:59 +0900

    Title: 최고의 13위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240613/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int num[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>num[i][j];
+        }
+    }
+    int max_coin=0;
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=0;j<=n-3;j++){
+            sum+=num[i][j];
+            sum+=num[i][j+1];
+            sum+=num[i][j+2];
+        }
+        max_coin=max(max_coin,sum);
+    }
+    cout<<max_coin;
+    return 0;
+}
