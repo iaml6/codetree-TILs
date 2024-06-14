c49751f4f91f73e4d9e189b0d9065d3824ba58d8
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-06-14 14:43:21 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240614/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 20
+using namespace std;
+
+int n;
+int num[MAX_N][MAX_N];
+
+
+
+int main() {
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>num[i][j];
+        }
+    }
+
+    int max_coin=0;
+    int firsti,firstj,firstij;
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=0;j<n-2;j++){
+            sum+=num[i][j];
+            sum+=num[i][j+1];
+            sum+=num[i][j+2];
+            firsti=num[i][j];
+            firstj=num[i][j+1];
+            firstij=num[i][j+2];
+            num[i][j]=0;
+            num[i][j+1]=0;
+            num[i][j+2]=0;
+            for(int k=0;k<n;k++){
+                for(int l=0;l<n-2;l++){
+                    sum+=num[k][l];
+                    sum+=num[k][l+1];
+                    sum+=num[k][l+2];
+                    max_coin=max(max_coin,sum);
+                }
+            }
+            num[i][j]=firsti;
+            num[i][j+1]=firstj;
+            num[i][j+2]=firstij;
+        }
+    }
+    cout<<max_coin;
+    return 0;
+}
