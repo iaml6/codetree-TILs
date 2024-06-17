db1a6e9b24c47dae879fa782d137f563a2a4f7ad
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-06-17 11:50:17 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240617/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 100
+using namespace std;
+int n;
+int a1,b1,c1,a2,b2,c2;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+    int cnt=0;
+    cin>>a1>>b1>>c1>>a2>>b2>>c2;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            
+            for(int k=1;k<=n;k++){
+                bool succed=false;
+                bool as=false,bs=false,cs=false;
+
+                for(int q=0;q<=2;q++){
+                    int x=i+q;
+                    int y=j+q;
+                    int z=k+q;
+                    if(x>n){
+                        x-=n;
+                    }
+                    if(y>n){
+                        y-=n;
+                    }
+                    if(z>n){
+                        z-=n;
+                    }
+                    if(x==a1){
+                        as=true;
+                    }
+                    if(y==b1){
+                        bs=true;
+                    }
+                    if(z==c1){
+                        cs=true;
+                    }
+                }
+
+                for(int q=0;q<=2;q++){
+                    int x=i-q;
+                    int y=j-q;
+                    int z=k-q;
+                    if(x<0){
+                        x+=n;
+                    }
+                    if(y<0){
+                        y+=n;
+                    }
+                    if(z<0){
+                        z+=n;
+                    }
+                    if(x==a1){
+                        as=true;
+                    }
+                    if(y==b1){
+                        bs=true;
+                    }
+                    if(z==c1){
+                        cs=true;
+                    }
+                }
+
+                if(as==true&&bs==true&&cs==true){
+                    succed=true;
+                }
+                as=false;
+                bs=false;
+                cs=false;
+
+                for(int q=0;q<=2;q++){
+                    int x=i+q;
+                    int y=j+q;
+                    int z=k+q;
+                    if(x>n){
+                        x-=n;
+                    }
+                    if(y>n){
+                        y-=n;
+                    }
+                    if(z>n){
+                        z-=n;
+                    }
+                    if(x==a2){
+                        as=true;
+                    }
+                    if(y==b2){
+                        bs=true;
+                    }
+                    if(z==c2){
+                        cs=true;
+                    }
+                }
+
+                for(int q=0;q<=2;q++){
+                    int x=i-q;
+                    int y=j-q;
+                    int z=k-q;
+                    if(x<0){
+                        x+=n;
+                    }
+                    if(y<0){
+                        y+=n;
+                    }
+                    if(z<0){
+                        z+=n;
+                    }
+                    if(x==a2){
+                        as=true;
+                    }
+                    if(y==b2){
+                        bs=true;
+                    }
+                    if(z==c2){
+                        cs=true;
+                    }
+                }
+
+                if(as==true&&bs==true&&cs==true){
+                    succed=true;
+                }
+
+
+                if(succed==true){
+                cnt++;
+                }
+            }
+
+            
+        }
+    }
+    cout<<cnt;
+
+    return 0;
+}
