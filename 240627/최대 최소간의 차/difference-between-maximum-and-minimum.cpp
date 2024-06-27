4852ed8e52c8b9d094d50d9fe1bcc651000a22b3
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-06-27 14:30:26 +0900

    Title: 최대 최소간의 차, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240627/최대 최소간의 차/difference-between-maximum-and-minimum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <algorithm>
+#define MAX_N 100
+#define MAX_K 10000
+using namespace std;
+
+int n,k;
+int num[MAX_N];
+int min_num= INT_MAX;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+        cin>>num[i];
+    }
+
+    for(int i=1;i<=10;i++){
+        int sum=0;
+        for(int j=0;j<n;j++){
+            if((num[j]-i)>k){
+                 sum+=(abs(i-num[j])-k);
+            }
+            if(num[j]<i){
+                sum+=(i-num[j]);
+            }
+            
+        }
+        
+        min_num=min(min_num,sum);
+    }
+    cout<<min_num;
+    return 0;
+}
