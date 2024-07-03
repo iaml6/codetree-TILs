dfaaecf4ad80b705f87cf1edf591fe20f0dec592
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-07-03 16:47:49 +0900

    Title: 최대 H 점수 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240703/최대 H 점수 2/maximum-h-score-2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_N 100
+using namespace std;
+
+int n,l;
+int num[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n>>l;
+    for(int i=0;i<n;i++){
+        cin>>num[i];
+    }
+
+  
+    int max_num=0;
+    for(int i=1;i<=n;i++){
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(num[j]>=i){
+                count++;
+                
+            }
+            
+        }
+        
+
+        if(count>=i){
+            max_num=i;
+        }
+    }
+    if(l==0){
+        cout<<max_num;
+        return 0;
+    }
+    cout<<max_num+1;
+
+    return 0;
+}
