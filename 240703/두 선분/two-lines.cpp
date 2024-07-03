1da1ee23b6b2f11d64dcacdf305a83537ac3e6aa
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-07-03 20:04:37 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240703/두 선분/two-lines.cpp
+++ 240703/두 선분/two-lines.cpp
@@ -5,10 +5,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int x1,x2,x3,x4;
     cin>>x1>>x2>>x3>>x4;
-    if(x2>x3){
+    if(x2>=x3){
         cout<<"intersecting";
     }
-    else if(x1<x4&&x1>x3){
+    else if(x1<=x4&&x1>=x3){
         cout<<"intersecting";
     }
     else{
