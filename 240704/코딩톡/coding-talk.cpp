aab933000c05abb8747cda88c9b33f167302a02c
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-07-04 15:07:11 +0900

    Title: 코딩톡, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240704/코딩톡/coding-talk.cpp
+++ 240704/코딩톡/coding-talk.cpp
@@ -10,18 +10,22 @@ int main() {
     // 여기에 코드를 작성해주세요.
     cin>>n>>m>>p;
     char max_gab='0';
+    int count[n]={};
     for(int i=0;i<m;i++){
         cin>>alpa[i]>>noread[i];
         max_gab=max(max_gab,alpa[i]);
+        count[alpa[i]-65]++;
     }
 
     int read[n]={};
     for(int i=p-1;i<m;i++){
         read[alpa[i]-65]++;
     }
+    
     int real = max_gab-65;
+    
     for(int i=0;i<=real;i++){
-        if(read[i]==0){
+        if(read[i]==0&&count[i]!=0){
             char after=i+'A';
             cout<<after<<" ";
         }
