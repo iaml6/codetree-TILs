19274694c1237ff48701b5a94e88650c5921ec47
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-07-08 16:00:58 +0900

    Title: 독서실의 거리두기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240708/독서실의 거리두기 3/study-cafe-keeping-distance-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_N 1000
+using namespace std;
+
+int n;
+char seat[MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>seat[i];
+    }
+    int min_dis=3000;
+    int max_dis=-1;
+    int dis=0;
+    for(int i=1;i<n-1;i++){
+        if(seat[i]=='1'){
+            max_dis=max(max_dis,dis);
+            min_dis=min(min_dis,dis);
+            dis=0;
+        }
+        else{
+            dis++;
+        }
+    }
+    if(max_dis==min_dis){
+        cout<<max_dis-1;
+    }
+    else {
+        cout<<min_dis;
+    }
+
+
+    return 0;
+}
