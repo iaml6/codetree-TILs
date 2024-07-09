0927bf235c64c8a28bc6855d9e34da1bca4ed05d
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-07-09 16:21:05 +0900

    Title: L, R 그리고 B, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240709/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+char num[10][10];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int l[2],b[2],r[2];
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            cin>>num[i][j];
+            if(num[i][j]=='B'){
+                b[0]=i;
+                b[1]=j;
+            }
+            else if(num[i][j]=='L'){
+                l[0]=i;
+                l[1]=j;
+            }
+            else if(num[i][j]=='R'){
+                r[0]=i;
+                r[1]=j;
+            }
+        }
+    }
+
+    if(l[0]==b[0]&&l[0]==r[0]){
+        cout<<abs(l[1]-b[1]);
+    }
+    else if(l[1]==b[1]&&l[1]==r[1]){
+        cout<<abs(l[0]-b[0]);
+    }
+    else{
+        cout<<abs(l[0]-b[0])+abs(l[1]-b[1])-1;
+    }
+
+    
+    return 0;
+}
