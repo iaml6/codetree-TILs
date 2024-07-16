b5ce34e1bbc77d0d452e8f86e355e9d1c979fa23
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-07-16 15:06:49 +0900

    Title: 수열의 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/수열의 순서 바꾸기/reorder-sequence.cpp
+++ 240716/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -12,13 +12,17 @@ int main() {
     }
     int end=-1;
     for(int i=0;i<n-1;i++){
-        int correct=true;
+        
+        int count=0;
         for(int j=i+1;j<n;j++){
-            if(num[j]>num[i]){
-                correct=false;
+            if(num[j]<num[i]){
+                count++;
             }
+            
         }
-        if(correct==true){
+        
+
+        if(count>0){
             end=i;
         }
     }
