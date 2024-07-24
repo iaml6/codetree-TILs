ee62b88f2b9420485f859cf81e36db64088043cb
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-07-24 22:09:58 +0900

    Title: 기울어진 직사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240724/기울어진 직사각형/slanted-rectangle.cpp
+++ 240724/기울어진 직사각형/slanted-rectangle.cpp
@@ -15,42 +15,47 @@ int main() {
         }
     }
     int sum=0;
-    
+    int first[n][n]={};
+    int second[n][n]={};
     for(int i=2;i<n;i++){
         for(int j=1;j<n-1;j++){
             int gab=0;
             
-            int first=0;
-            int x=i,y=j;
-            while(y<n-1&&x>1){
-                x-=1;
-                y+=1;
-                first++;
-                
-                gab+=square[x][y];
-            }
-            int second=0;
-            while(x>0){
-                x-=1;
-                y-=1;
-                second++;
-                
-                gab+=square[x][y];
-            }
-            for(int k=0;k<first;k++){
-                x+=1;
-                y-=1;
-                
-                gab+=square[x][y];
-            }
-            for(int k=0;k<second;k++){
-                x+=1;
-                y+=1;
-                
-                gab+=square[x][y];
-            }
             
-            if(second>0&&first>0){
+            int x=i,y=j;
+            int a=n-1;
+            int b=1;
+            
+                
+                    while(y<a&&x>b){
+                        x-=1;
+                        y+=1;
+                        first[i][j]++;
+                        
+                        gab+=square[x][y];
+                    }
+                    
+                    while(x>b-1){
+                        x-=1;
+                        y-=1;
+                        second[i][j]++;
+                        
+                        gab+=square[x][y];
+                    }
+                    for(int k=0;k<first[i][j];k++){
+                        x+=1;
+                        y-=1;
+                        
+                        gab+=square[x][y];
+                    }
+                    for(int k=0;k<second[i][j];k++){
+                        x+=1;
+                        y+=1;
+                        
+                        gab+=square[x][y];
+                    }
+            
+            if(second[i][j]>0&&first[i][j]>0){
                 
                 sum=max(sum,gab);
             }
@@ -61,6 +66,57 @@ int main() {
         
 
     }
+    for(int i=2;i<n;i++){
+        for(int j=1;j<n-1;j++){
+            int gab=0;
+            int count1=0,count2=0;
+            int x=i,y=j;
+            cout<<"first["<<i<<"]["<<j<<"] "<<first[i][j]<<" second["<<i<<"]["<<j<<"]  "<<second[i][j]<<"  ";
+            for(int k=1;k<=first[i][j];k++){
+                gab=0;
+                count1=0,count2=0;
+                x=i,y=j;
+                for(int l=1;l<=k;l++){
+                    x-=1;
+                    y+=1;
+                    count1++;
+                    gab+=square[x][y];
+                }
+                int currentx=x, currenty=y,currentgab=gab;
+                while((currentx-1)>=0&&(currenty-1)>=0){
+                    x=currentx;
+                    y=currenty;
+                    gab=currentgab;
+                    x--;
+                    y--;
+                    count2++;
+                    gab+=square[x][y];
+                    currentgab=gab;
+                    currentx=x;
+                    currenty=y;
+                    for(int l=0;l<count1;l++){
+                        x+=1;
+                        y-=1;
+                        gab+=square[x][y];
+                    }
+                    for(int l=0;l<count2;l++){
+                        x+=1;
+                        y+=1;
+                        gab+=square[x][y];
+                    }
+                    if(
+                    count1>0&&count2>0){
+                    cout<<gab<<endl;
+                    sum=max(sum,gab);
+                    }
+                }
+                
+                
+                
+            }
+        }
+    }
+    
     cout<<sum;
     return 0;
 }
