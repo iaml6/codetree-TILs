47a028d4e3df3cd8f041e9c78ef7cf1ec8d81e38
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-07-29 14:05:36 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240729/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_N 5
+using namespace std;
+
+int n,m;
+int square[MAX_N][MAX_N];
+
+int Second(int start_x,int start_y,int end_x,int end_y,int a, int b){
+    int sum=-100000;
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            if((i>=start_x&&i<=end_x)&&(j>=start_y&&j<=end_y)) continue;
+            for(int k=i;k<a;k++){
+                for(int l=j;l<b;l++){
+                    int hab=0;
+                    for(int c=i;c<=k;c++){
+                        
+                        for(int d=j;d<=l;d++){
+                            hab+=square[c][d];
+                        }
+
+                    }
+                    sum=max(sum,hab);
+                }
+            }
+        }
+    }
+    return sum;
+}
+
+int Square(int a,int b){
+    int sum=-100000;
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            for(int k=i;k<a;k++){
+                for(int l=j;l<b;l++){
+                    int hab=0;
+                    for(int c=i;c<=k;c++){
+                        
+                        for(int d=j;d<=l;d++){
+                            hab+=square[c][d];
+                        }
+
+                    }
+                    hab+=Second(i,j,k,l,a,b);
+
+                    sum=max(sum,hab);
+
+
+                }
+            }
+        }
+    }
+
+    return sum;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>square[i][j];
+        }
+    }
+
+    
+    cout<<Square(n,m);
+
+    return 0;
+}
