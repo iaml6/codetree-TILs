5c39202543f1beeb6f3b29eff3dff7673d65acd6
iaml6 <155732959+iaml6@users.noreply.github.com>
2024-08-01 12:51:22 +0900

    Title: 십자 모양 폭발, Time: 19ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/십자 모양 폭발/cross-shape-bomb.cpp
+++ 240801/십자 모양 폭발/cross-shape-bomb.cpp
@@ -17,6 +17,7 @@ int main() {
     }
     cin>>r>>c;
     int len=square[r][c];
+    
     for(int i=0;i<len;i++){
         if(r+i<=n){
             square[r+i][c]=0;
@@ -28,15 +29,16 @@ int main() {
             square[r][c-i]=0;
         }
         if(r-i>=1){
-            square[r-1][c]=0;
+            square[r-i][c]=0;
         }
     }
     
-    int new_square[n][n]={};
-    for(int j=1;j<=n;j++){
+    int new_square[n+1][n+1]={};
+    
+    for(int j=n;j>=1;j--){
         int start=n;
          for(int i=n;i>=1;i--){
-        
+            
         
             if(square[i][j]!=0){
                 new_square[start][j]=square[i][j];
@@ -46,6 +48,14 @@ int main() {
         
         }
     }
+    
+    
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n;j++){
+    //         cout<<square[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             cout<<new_square[i][j]<<" ";
